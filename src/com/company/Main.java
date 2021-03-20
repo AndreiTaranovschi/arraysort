146ad5d91417cfc797a77af9551c759e31f60681
Андрей Тарановский <unysonic@MacBook-Pro-Andrej.local>
2021-03-20 14:10:29 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        char[] myArray = {'t','a','r','a','n','o','v','s','c','h'};
+
+
+        Arrays.sort(myArray);
+        System.out.println(myArray);
+
+    }
+}
